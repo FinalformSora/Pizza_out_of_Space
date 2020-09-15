ed098b8dc68043d3ce34cda26e6f3c82635b806e
stanleydo <stanleydo100@gmail.com>
2020-09-15 15:09:46 -0700

    Added some starting scripts, level layout

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController controller;
+
+    public float jumpSpeed = 400f;
+    public float gravity = 9.8f;
+
+    private float vSpeed = 0f;
+
+    // Movespeed
+    public float speed = 12f;
+
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        if (controller.isGrounded)
+        {
+            vSpeed = 0f;
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                vSpeed = jumpSpeed * Time.fixedDeltaTime;
+            }
+        }
+
+        vSpeed -= gravity * Time.fixedDeltaTime;
+
+        Vector3 movement = transform.right * x * speed * Time.deltaTime + transform.forward * z * speed * Time.deltaTime;
+        movement.y = vSpeed * Time.fixedDeltaTime;
+
+        controller.Move(movement);
+    }
+}
