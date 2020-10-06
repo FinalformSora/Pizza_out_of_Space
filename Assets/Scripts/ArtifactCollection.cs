e057228fe3aa1b7b659c973234ad7355d7abef4b
stanleydo <stanleydo100@gmail.com>
2020-10-06 14:48:43 -0700

    Added collectible objects, dynamic object generation, base ui

---
+++ Assets/Scripts/ArtifactCollection.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ArtifactCollection : MonoBehaviour
+{
+
+    public Transform playerCam;
+    public float rcDistance = 4f;
+    public Text artifactCountText;
+    public Text artifactCollectText;
+
+    public bool interactTextState = false;
+
+    private int artifactCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        artifactCount = 0;
+        artifactCollectText.gameObject.SetActive(interactTextState);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Bit shift the index of the layer (8) to get a bit mask
+        int layerMask = 1 << 8;
+
+        // This would cast rays only against colliders in layer 8.
+        // But instead we want to collide against everything except layer 8. The ~ operator does this, it inverts a bitmask.
+        layerMask = ~layerMask;
+
+        RaycastHit hit;
+        // Does the ray intersect any objects excluding the player layer
+        if (Physics.Raycast(playerCam.position, playerCam.TransformDirection(Vector3.forward), out hit, rcDistance, layerMask))
+        {
+            // Debug.DrawRay(playerCam.position, playerCam.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+
+            if (hit.collider.gameObject.name.Contains("Collectible"))
+            {
+                interactTextState = true;
+
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    artifactCount++;
+                    artifactCountText.text = artifactCount.ToString();
+                    hit.collider.gameObject.SetActive(false);
+                    interactTextState = false;
+                }
+            } 
+        }
+
+        artifactCollectText.gameObject.SetActive(interactTextState);
+        interactTextState = false;
+    }
+}
---
+++ Assets/Scripts/DynamicCollectibleCreation.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEditor;
+using UnityEngine;
+
+public class DynamicCollectibleCreation : MonoBehaviour
+{
+    public Transform parent;
+
+    public int numCollectibles = 10;
+    public Tuple xRange;
+    public Tuple yRange;
+    public Tuple zRange;
+
+    public Material m_MainMaterial;
+
+    public float cubeSize = 0.5f;
+    private Vector3 cubeVector;
+
+    private int[] collectibles;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cubeVector = new Vector3(cubeSize, cubeSize, cubeSize);
+        collectibles = new int[numCollectibles];
+        for (int i = 0; i < numCollectibles; i++)
+        {
+            GameObject collectible = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            collectible.transform.localScale = cubeVector;
+            float xPos = UnityEngine.Random.Range(xRange.min, xRange.max);
+            float yPos = UnityEngine.Random.Range(cubeSize/2, yRange.max);
+            float zPos = UnityEngine.Random.Range(zRange.min, zRange.max);
+            collectible.transform.position = new Vector3(xPos, yPos, zPos);
+            collectible.gameObject.name = "Collectible";
+            collectible.gameObject.transform.parent = parent;
+            collectible.GetComponent<Renderer>().material = m_MainMaterial;
+
+
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    [Serializable]
+    public class Tuple
+    {
+        public float min;
+        public float max;
+    }
+}
