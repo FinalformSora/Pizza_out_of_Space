cb98c68d43c9642063fd48a58e77e6771764f9a2
Jesus Osuna <jesusgonzalez9664@gmail.com>
2020-10-18 22:54:39 -0700

    added stairs

---
+++ Assets/Scripts/CodyHealth.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CodyHealth : MonoBehaviour
+{
+    [SerializeField] float hitPoints = 1f;
+
+    //Take damage
+    public void TakeDamage(float damage)
+    {
+        hitPoints -= damage;
+        if (hitPoints <= 0)
+        {
+            //Debug.Log("You dead, my Glip glop");
+            GetComponent<LosingScreen>().HandleDeath();
+        }
+    }
+}
---
+++ Assets/Scripts/EnemyAttack.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    CodyHealth target;
+    [SerializeField] float damage = 40f;
+
+    KlownAi enemyAi;
+
+    void Start()
+    {
+        enemyAi = FindObjectOfType<KlownAi>();
+        target = FindObjectOfType<CodyHealth>();
+    }
+
+    public void AttackHitEvent()
+    {
+        if (target == null) return;
+        //    if (target.dis)
+        target.TakeDamage(damage);
+        Debug.Log("Bang Bang");
+    }
+}
