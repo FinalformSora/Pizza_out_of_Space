91cc0d7483408dd6b1924bfd4d53fefd2c10e30b
stanleydo <stanleydo100@gmail.com>
2020-10-30 10:36:31 -0700

    Fixed lights, added some tables and stuff

--- Assets/Scripts/FootSteps.cs
+++ Assets/Scripts/FootSteps.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class FootSteps : MonoBehaviour
 {
@@ -9,37 +11,50 @@ public class FootSteps : MonoBehaviour
 
     public AudioSource feet;
 
-    public AudioClip[] clips_walking;
-    public AudioClip[] clips_running;
+    public AudioClip clip_walking;
+    public AudioClip clip_running;
+    public AudioClip clip_jumping;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        feet.clip = clip_running;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if ((player.velocity.x != 0 || player.velocity.z != 0) && player.velocity.y == 0)
-        {
-            if (Input.GetKey(KeyCode.LeftShift))
-            {
-                playFootsteps(clips_running);
-            }
-
-            playFootsteps(clips_walking);
-        }
-    }
-
-    void playFootsteps(AudioClip[] clips)
-    {
         if (!feet.isPlaying)
-        {
-            AudioClip nextStep = clips[Random.Range(0, clips.Length)];
-            feet.clip = nextStep;
             feet.Play();
 
+        if ((player.velocity.x != 0 || player.velocity.z != 0) && player.velocity.y < 0.1)
+        {
+
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                if (feet.isPlaying && (feet.clip != clip_running))
+                {
+                    feet.Stop();
+                    feet.clip = clip_running;
+                }
+            } else
+            {
+                if (feet.isPlaying && (feet.clip == clip_running || feet.clip == clip_jumping))
+                {
+                    feet.Stop();
+                    feet.clip = clip_walking;
+                }
+            }
         }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            feet.Stop();
+            feet.clip = clip_jumping;
+        }
+
+        if (player.velocity.x == 0 && player.velocity.z == 0 && player.velocity.y < 0.1)
+            feet.Stop();
+
     }
 }
