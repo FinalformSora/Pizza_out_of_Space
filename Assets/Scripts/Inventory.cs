df7388d4c5530ce333fa9a0e4a51debdd060db08
EilonwyLlyr <jonathaneilonwynunez@gmail.com>
2020-12-04 22:23:35 -0800

    Changed locations of certain monsters and updated atrifact

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -10,10 +10,10 @@ public class Inventory
     {
         itemList = new List<Item>();
 
-        AddItem(new Item { itemType = Item.ItemType.Repel, amount = 0, cost = 10 });
-        AddItem(new Item { itemType = Item.ItemType.Attract, amount = 0, cost = 10 });
+        AddItem(new Item { itemType = Item.ItemType.Repel, amount = 5, cost = 10 });
+        AddItem(new Item { itemType = Item.ItemType.Attract, amount = 5, cost = 10 });
         AddItem(new Item { itemType = Item.ItemType.Trap, amount = 0, cost = 20 });
-        AddItem(new Item { itemType = Item.ItemType.Battery, amount = 0, cost = 5 });
+        AddItem(new Item { itemType = Item.ItemType.Battery, amount = 5, cost = 5 });
         AddItem(new Item { itemType = Item.ItemType.Pizza, amount = 0, cost = 0 });
     }
 
